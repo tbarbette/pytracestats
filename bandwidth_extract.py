1ad811c6df359078d10f6eb99fe480e30efa46a3
Tom Barbette <barbette@kth.se>
2019-09-04 14:55:08 +0200

    Improve help

--- bandwidth_extract.py
+++ bandwidth_extract.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import pygal
 import os
 from datetime import datetime
@@ -7,17 +7,24 @@ import argparse
 from tqdm import tqdm
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filename",default=["dump.pcap"], nargs='+')
-    parser.add_argument("--interval",type=float,default=0.1)
-    parser.add_argument("--stop-after",dest='stop',type=float,default=None)
-    parser.add_argument("--output",type=str,default=None)
+    parser = argparse.ArgumentParser("Generate interactive SVG displaying the bandwidth of one or multiple PCAP files")
+    parser.add_argument("filename",default=["dump.pcap"], nargs='+', help="Name(s) of the PCAP files to parse.")
+    parser.add_argument("--interval",type=float,default=0.1,help="Interval to compute the bandwidth upon. Defaults to 0.1 seconds.")
+    parser.add_argument("--stop-after",dest='stop',type=float,default=None,help="Stop after a certain amount of seconds.")
+    parser.add_argument("--output",type=str,default=None,help="Output filename. Defaults to the name of the last input PCAP file, changing the extension by SVG.")
     args = parser.parse_args()
+
+    #Manipulate filenames to have meaning full display names
+    common = os.path.commonprefix(args.filename)
+    if min([len(filename[len(common):]) for filename in args.filename]) == 0:
+        common = os.path.commonpath(args.filename)
 
     chart = pygal.XY(stroke=True, interpolate='hermite', y_title="Bandwidth (Gbits/s)", x_title="Time (s)")
     for filename in args.filename:
-        print("Processing %s..." % filename)
         trace = PcapReader(filename)
+
+        cname = filename[len(common):].strip(os.path.sep)
+        print("Processing %s..." % cname)
 
         npackets = 0
         last = 0
@@ -30,6 +37,7 @@ def main():
 
         with tqdm(total=os.stat(filename).st_size, desc="Reading trace...", bar_format="{l_bar}{bar} [ time left: {remaining} ]", dynamic_ncols=True) as pbar:
           for packet in trace:
+              try:
                 pos = trace.f.tell()
                 pbar.update(pos - last)
                 last = pos
@@ -54,8 +62,9 @@ def main():
                     last_time = ptime
                     tot = 0
                     n = 0
-
-        chart.add(filename, [(t,b / 1000000000) for t,b in bw])
+              except Exception as e:
+                print("WARNING: Packet ignored")
+        chart.add(cname, [(t,b / 1000000000) for t,b in bw])
         #chart.x_labels = [t for t,b in bw]
 
 
